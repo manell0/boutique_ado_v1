89c2335e9b15cdd2ee2dbf2c1a778079bcba2503
manell0 <svante.magnell@gmail.com>
2021-08-19 05:35:48 +0000

    Add checkout/static/js folder stripe.js

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JPqYqKEQL252kDtnJMFQJYisHP1LtKyznPszLJVnibO12vygCeEQIYYiXJ553mGIyHcoVHG6Nv1R7uSgf8B2Z0g00SXUtYUb0',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
