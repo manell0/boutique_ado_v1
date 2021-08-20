0e147f3f7b5e9272434b6fdd95e3b05de6a32e6f
manell0 <svante.magnell@gmail.com>
2021-08-20 12:59:25 +0000

    Update error handling

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -26,13 +26,18 @@ def webhook(request):
         )
     except ValueError as e:
         # Invalid payload
+        print('--------------------------')
+        print('Invalid payload 1')
+        print('--------------------------')
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
+        print('--------------------------')
+        print('Invalid signature 2', e)
+        print('--------------------------')
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
